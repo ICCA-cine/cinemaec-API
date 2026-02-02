e8fff60ff8049960d93a091378853f3eca9c1433
attisDev92 <attis.alejandro@gmail.com>
2026-02-01 21:30:15 -0500

    fix: correct array column type in movie_content_bank migration

--- src/migrations/1769634442233-CreateMovieContentBankTable.ts
+++ src/migrations/1769634442233-CreateMovieContentBankTable.ts
@@ -7,7 +7,7 @@ export class CreateMovieContentBankTable1769634442233
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "movie_content_bank" ("id" SERIAL NOT NULL, "movieId" integer NOT NULL, "licensingStartDate" date NOT NULL, "licensingEndDate" date NOT NULL, "exhibitionWindow" character varying array NOT NULL, "geolocationRestrictionCountryId" integer, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_e35f9b955ef8b3140c31a8363c4" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "movie_content_bank" ("id" SERIAL NOT NULL, "movieId" integer NOT NULL, "licensingStartDate" date NOT NULL, "licensingEndDate" date NOT NULL, "exhibitionWindow" character varying[] NOT NULL, "geolocationRestrictionCountryId" integer, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_e35f9b955ef8b3140c31a8363c4" PRIMARY KEY ("id"))`,
     )
     await queryRunner.query(
       `ALTER TABLE "movie_content_bank" ADD CONSTRAINT "FK_a464ed02fbe56d962cabd685b9c" FOREIGN KEY ("movieId") REFERENCES "movies"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
